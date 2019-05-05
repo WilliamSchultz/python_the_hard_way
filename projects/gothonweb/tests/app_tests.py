ff7a32b2f65d135ce8514deac2a0015eed545fc0
William <williamterrillschultz@gmail.com>
2019-05-05 22:00:34 +0300

    Ex51

--- projects/gothonweb/tests/app_tests.py
+++ projects/gothonweb/tests/app_tests.py
@@ -3,15 +3,12 @@ from bin.app import app
 from tests.tools import assert_response
 
 def test_index():
-    # check that we get a 404 on the / URLresp = app.request("/")
-    assert_response(resp, status="404")
-    # test our first GET request to /hello
-    resp = app.request("/hello")
-    assert_response(resp)
-    # make sure default values work for the form
-    resp = app.request("/hello", method="POST")
-    assert_response(resp, contains="Nobody")
-    # test that we get expected values
-    data = ['name': 'Zed', 'greet': 'Hola']
-    resp = app.request("/hello", method="POST", data=data)
-    assert_response(resp, contains="Zed")
+  resp = app.request("/")
+  assert_response(resp, status="404")
+  resp = app.request("/hello")
+  assert_response(resp)
+  resp = app.request("/hello", method="POST")
+  assert_response(resp, contains="Nobody")
+  data = {"name": "Zed", "greet":"Hola"}
+  resp = app.request("/hello", method="POST", data=data)
+  assert_response(resp, contains="Zed")
